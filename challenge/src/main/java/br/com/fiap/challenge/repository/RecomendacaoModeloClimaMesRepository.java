bb2f849dfd7c21d0ead7155eb3c1ad6c52ccabcb
Lucas Schroeder <lucasschroederkm@gmail.com>
2022-05-15 16:51:11 -0300

    Add sprint_1

---
+++ challenge/src/main/java/br/com/fiap/challenge/repository/RecomendacaoModeloClimaMesRepository.java
@@ -0,0 +1,9 @@
+package br.com.fiap.challenge.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.fiap.challenge.model.RecomendacaoModeloClimaMes;
+
+public interface RecomendacaoModeloClimaMesRepository extends JpaRepository<RecomendacaoModeloClimaMes, Integer> {
+
+}
