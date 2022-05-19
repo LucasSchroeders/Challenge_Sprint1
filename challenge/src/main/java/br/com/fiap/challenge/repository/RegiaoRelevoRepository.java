97df29fae55638c794a18db7962b2b77109cebdf
Lucas Schroeder <lucasschroederkm@gmail.com>
2022-05-18 21:03:29 -0300

    Retirando as interfaces das tabelas associtativas

--- challenge/src/main/java/br/com/fiap/challenge/repository/RegiaoRelevoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.challenge.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.fiap.challenge.model.ClimaMes;
-
-public interface RegiaoRelevoRepository extends JpaRepository<ClimaMes, Integer>{
-
-}
